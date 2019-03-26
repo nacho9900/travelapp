6652b0d24c4f2e4672df881da4ee08affe89d3dd
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2019-03-26 20:49:48 -0300

    Spring HelloWorld with some configs

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -1,4 +1,26 @@
 package ar.edu.itba.paw.webapp.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan({"ar.edu.itba.paw.webapp.controller"})
+@Configuration
 public class WebConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        final InternalResourceViewResolver vr = new InternalResourceViewResolver();
+
+        vr.setViewClass(JstlView.class);
+        vr.setPrefix("/WEB-INF/jsp/");
+        vr.setSuffix(".jsp");
+
+        return vr;
+    }
 }
