230c8983cf239ab03f55a2b49b92839f0cca7c44
FelipeGorostiaga <fgorostiaga@itba.edu.ar>
2019-03-30 01:30:58 -0300

    Main Controller

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/MainController.java
@@ -0,0 +1,46 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public ModelAndView index() { return new ModelAndView("index"); }
+
+    @RequestMapping("/home")
+    public ModelAndView home() { return new ModelAndView("home"); }
+
+    @RequestMapping("/signup")
+    public ModelAndView signup() { return new ModelAndView("signup"); }
+
+    @RequestMapping("/signin")
+    public ModelAndView signin() { return new ModelAndView("signin"); }
+
+    @RequestMapping(value = "/signin", method = {RequestMethod.POST})
+    public ModelAndView validateSignIn(@RequestParam String username,
+                                       @RequestParam String password) {
+        ModelAndView mav = new ModelAndView("home");
+        if(username.equals("paw") && password.equals("password")) {
+            mav.addObject("username", username);
+            return mav;
+        }
+        mav.setViewName("signin");
+        return mav;
+    }
+
+    @RequestMapping(value = "/signup", method = {RequestMethod.POST})
+    public ModelAndView validateSignUp(@RequestParam String first_name,
+                                       @RequestParam String last_name,
+                                       @RequestParam String email,
+                                       @RequestParam String password,
+                                       @RequestParam String psw_repeat ) {
+        ModelAndView mav = new ModelAndView("index");
+        return mav;
+    }
+
+}
--- webapp/src/main/webapp/WEB-INF/jsp/index.jsp
+++ webapp/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,5 +1,14 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt"%>
+
 <html>
+<head>
+	<title>Meet and Travel</title>
+</head>
 <body>
-<h2>Hello ${alo}!</h2>
+	<h1>Meet people and travel the world!<h1>
+	<div>
+		<a href="<c:url value='/signup' />">Sign up</a>
+		<a href="<c:url value='/signin' />" >Sign in</a>
+	</div>
 </body>
 </html>
