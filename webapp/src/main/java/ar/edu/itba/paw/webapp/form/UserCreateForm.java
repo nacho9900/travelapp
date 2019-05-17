f5e4d1130eed976dfab78f20c2bda6db3fd383f4
Florencia Monti <mamonti@itba.edu.ar>
2019-05-17 18:23:33 -0300

    Changes to i18n messages

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/UserCreateForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/UserCreateForm.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class UserCreateForm {
 
-    //TODO AGREGAR PATTERN A BIRTHDAY Y PSW REPEAT CHECKING
+    //TODO PSW REPEAT CHECKING
 
     @Pattern(regexp = "[a-zA-Z]+")
     @Size(min = 2, max = 100)
@@ -26,6 +26,7 @@ public class UserCreateForm {
     @Size(min = 8, max = 100)
     private String pswrepeat;
 
+    @Pattern(regexp = "^(?:(?:(?:0?[13578]|1[02])(/)31)\\1|(?:(?:0?[1,3-9]|1[0-2])(/)(?:29|30)\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:0?2(/)29\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(/)(?:0?[1-9]|1\\d|2[0-8])\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$")
     private String birthday;
 
     @Size(min = 2, max = 5)
